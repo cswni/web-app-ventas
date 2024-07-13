8ee3dccc890bcd2125dd614d9d469ebdf4987e44
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-07-12 22:24:00 -0600

    feat: Add meta tags and update vite configurations
    Meta tags have been added to the index.html for better SEO and social media sharing. Configuration for Vite has also been updated, adjusting the base route depending on the deployment environment, and changing the assets directory and output directory. The main input for the rollup options has also been modified.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import path from 'path';
+import path, { resolve } from 'path';
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const env = process.env;
+//Set a base route if deploying on github pages
+env.mode = env.mode || 'production';
+
+console.log('Environment: ', env.mode);
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
+  //Add base route if deploying on production
+  base: env.mode === 'production' ? '/web-app-ventas/' : '/',
   plugins: [react()],
 
   // Alias
@@ -20,5 +28,14 @@ export default defineConfig({
   // Expose server to network
   server: {
     host: '0.0.0.0'
+  },
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
   }
 });
