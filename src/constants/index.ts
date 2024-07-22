e125a4043af02b90337a1e764996ccd7b3826b41
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-07-21 19:32:13 -0600

    Add BASENAME constant from environment variable
    The BASENAME constant has been added to the constants/index.ts file and is imported from the environment variable VITE_BASENAME. This newly introduced constant is used in the BrowserRouter component in the main.tsx file to set the basename attribute dynamically. This allows for easier configurations across different environments.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const MODE = import.meta.env.VITE_MODE;
+export const BASENAME = import.meta.env.VITE_BASENAME;
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,11 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 
 import './assets/js/main.js';
+import { BASENAME } from './constants';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={'/web-app-ventas/'}>
+    <BrowserRouter basename={BASENAME}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
